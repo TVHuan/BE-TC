b0388381dac15080ad027a4224a5861b15cce6dd
TVHuan <tahuan1906@gmail.com>
2024-12-25 00:55:31 +0700

    BE

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,7 +2,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*", // Cho phép tất cả các nguồn gốc
+  origin: "https://check-bien-so-tai-nan.onrender.com", // Cho phép tất cả các nguồn gốc
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Cho phép tất cả các phương thức
   allowedHeaders: ["Content-Type", "Authorization"], // Cho phép các header này
 };
